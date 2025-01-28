083b9ecad48773e1bbb6202aa20540eb2d8cf14c
venkatasudha1771 <venkatasudha1771@gmail.com>
2025-01-29 10:26:51 +1100

    Add project files.

---
+++ DevOpsDemo/Repository/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using DevOpsDemo.Models;
+using System.Collections.Generic;
+
+namespace DevOpsDemo.Repository
+{
+    public interface IPostRepository
+    {
+        public interface IPostRepository
+        { 
+            List<PostViewModel> GetPosts();
+        }
+    }
+}
